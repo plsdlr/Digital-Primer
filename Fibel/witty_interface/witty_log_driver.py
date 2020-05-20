ee5b75e71cc8d45c7e7d95308789fc6def46c85f
p <paulseidler1@gmail.com>
2020-05-20 18:24:36 +0200

    small fix and rename

--- Fibel/witty_interface/witty_log_driver.py
+++ Fibel/witty_interface/witty_log_driver.py
@@ -3,7 +3,7 @@ from witty_logger import thread_logger as tl
 from witty_interface import wpi_interface as wi
 
 wpi = wi()
-functions_to_log = [wpi.read_v_in(), wpi.read_v_out(), wpi.read_cur_out()]
+functions_to_log = [wpi.read_v_in, wpi.read_v_out, wpi.read_cur_out]
 variables_to_log = ["v_in","v_out","cur_out"]
 timeintervall = 3
 path = 'testwrite.csv'
